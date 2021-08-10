658b1476aa16a5132eae71cd3503e86d1671a9fe
intisaar <intisaarothan@gmail.com>
2021-08-10 11:59:24 +0300

    third commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -42,8 +42,6 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -67,6 +65,32 @@
 										</li>
 									</ul>
 								</li>
+								@if(Route::has('login'))
+									@auth
+										@if(Auth::user()->utype === 'ADM')
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Dashboard" href="#">Dashboard</a>
+												</li>
+											</ul>
+											</li>
+										@else
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+											<ul class="submenu curency" >
+												<li class="menu-item" >
+													<a title="Dashboard" href="#">Dashboard</a>
+												</li>
+											</ul>
+											</li>
+										@endif
+									@else
+										<li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
+										<li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
+									@endif
+								@endif	
 							</ul>
 						</div>
 					</div>
@@ -76,7 +100,7 @@
 					<div class="mid-section main-info-area">
 
 						<div class="wrap-logo-top left-section">
-							<a href="index.html" class="link-to-home"><img src="{{ assets('assets/images/logo-top-1.png') }}" alt="mercado"></a>
+							<a href="/" class="link-to-home"><img src="{{ asset('assets/images/logo-top-1.png') }}" alt="mercado"></a>
 						</div>
 
 						<div class="wrap-search center-section">
@@ -159,19 +183,19 @@
 						<div class="container">
 							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
 								<li class="menu-item home-icon">
-									<a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
 									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
-									<a href="shop.html" class="link-term mercado-item-title">Shop</a>
+									<a href="/shop" class="link-term mercado-item-title">Shop</a>
 								</li>
 								<li class="menu-item">
-									<a href="cart.html" class="link-term mercado-item-title">Cart</a>
+									<a href="/cart" class="link-term mercado-item-title">Cart</a>
 								</li>
 								<li class="menu-item">
-									<a href="checkout.html" class="link-term mercado-item-title">Checkout</a>
+									<a href="/checkout" class="link-term mercado-item-title">Checkout</a>
 								</li>
 								<li class="menu-item">
 									<a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +22,14 @@ use App\Http\Livewire\HomeComponent;
 //   return view('welcome');
 //   });
 
-Route::get('/',HomeComponent::Class);
+Route::get('/',HomeComponent::class);
+
+Route::get('/shop',ShopComponent::class);
+
+Route::get('/cart',CartComponent::class);
+
+Route::get('/checkout',CheckoutComponent::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
