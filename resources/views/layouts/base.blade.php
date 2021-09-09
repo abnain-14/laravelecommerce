8fd2c2df03324f8e53f65f4f3e0e2bb9bfa15014
intisaar <intisaarothan@gmail.com>
2021-09-09 11:36:37 +0300

    add WYSIWYG EDITOR ON PRODUCT PAGE

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -461,6 +461,9 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js" integrity="sha512-GDey37RZAxFkpFeJorEUwNoIbkTwsyC736KNSYucu1WJWFK9qTdzYub8ATxktr6Dwke7nbFaioypzbDOQykoRg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.7.0/nouislider.min.js" integrity="sha512-jWNpWAWx86B/GZV4Qsce63q5jxx/rpWnw812vh0RE+SBIo/mmepwOSQkY2eVQnMuE28pzUEO7ux0a5sJX91g8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	
+	<script src="https://cdn.tiny.cloud/1/oho874njnxqho1w21rkuqiwz1k6y3hao5sdqfeg87pv8macf/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
 	@livewireScripts
 
 	@stack('scripts')
