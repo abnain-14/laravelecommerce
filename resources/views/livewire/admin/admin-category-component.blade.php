c7048980182d86220dd452b434b9b71d451f3540
intisaar <intisaarothan@gmail.com>
2021-09-09 10:52:51 +0300

    confirm deletion

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -42,7 +42,7 @@
                                     <td>{{$category->slug}}</td>
                                     <td>
                                         <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"><i  class="fa fa-edit fa-2x"></i>Edit</a>
-                                        <a href="#" wire:click.prevent="deleteCategory({{$category->id}})"><i class="fa fa-times fa-2x text-danger"></i>Delete</a>
+                                        <a href="#" onclick="confirm('Are you sure, You want to delete this category?') || event.stopImmediatePropagation()" wire:click.prevent="deleteCategory({{$category->id}})"><i class="fa fa-times fa-2x text-danger"></i>Delete</a>
                                     </td>
                                 </tr>
                                 @endforeach
