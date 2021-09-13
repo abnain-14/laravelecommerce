80693bbd9e42f5c3737411db97d178dffb1fc96f
intisaar <intisaarothan@gmail.com>
2021-09-13 09:52:58 +0300

    quantyt

--- app/Http/Livewire/WishlistComponent.php
+++ app/Http/Livewire/WishlistComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Cart;
+use App\Model\Product;
 
 class WishlistComponent extends Component
 {
@@ -18,6 +19,16 @@ class WishlistComponent extends Component
                 return;
             }
         }
+    }
+
+    public function moveProductFromWishlistToCart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->emitTo('wishlist-count-component','refreshComponent');
+        $this->emitTo('cart-count-component','refreshComponent');
+
     }
 
     public function render()
