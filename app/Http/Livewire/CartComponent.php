a63760125aeae3b6a27d850810f6adb8be3380a7
intisaar <intisaarothan@gmail.com>
2021-09-17 12:36:51 +0300

    Cart Settings for Checkout

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use Carbon\Carbon;
 use Cart;
 use App\Models\Coupon;
+use Illuminate\Support\Facades\Auth;
 
 class CartComponent extends Component
 {
@@ -108,6 +109,39 @@ class CartComponent extends Component
         session()->forget('coupon');
     }
 
+    public function checkout() {
+
+        if (Auth::check())
+        {
+            return redirect()->to('/checkout');
+        }
+        else {
+            return redirect()->route('login');
+        }
+    }
+
+    public function setAmountForCheckout()
+    {
+        if(session()->has('coupon'))
+        {
+            session()->put('checkout',[
+                'discount' => $this->discount,
+                'subtotal' => $this->subtotalAfterDiscount,
+                'tax' => $this->taxAfterDiscount,
+                'total' => $this->totalAfterDiscount
+            ]);
+        }
+        else
+        {
+            session()->put('checkout',[
+                'discount' => 0,
+                'subtotal' => Cart::instance('cart')->subtotal(),
+                'tax' => Cart::instance('cart')->tax(),
+                'total' => Cart::instance('cart')->total()
+            ]);
+        }
+    }
+
     public function render()
     {
         if(session()->has('coupon'))
@@ -121,6 +155,7 @@ class CartComponent extends Component
                 $this->calculateDiscounts();
             }
         }
+        $this->setAmountForCheckout();
         return view('livewire.cart-component')->layout("layouts.base");
     }
 }
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -9,6 +9,7 @@
         </ul>
     </div>
     <div class=" main-content-area">
+        @if(Cart::instance('cart')->count() > 0)
 
         <div class="wrap-iten-in-cart">
             @if(Session::has('success_message'))
@@ -88,7 +89,7 @@
                 </div>
                 @endif
                 @endif
-                <a class="btn btn-checkout" href="checkout.html">Check out</a>
+                <a class="btn btn-checkout" href="#" wire:click.prevent="checkout">Check out</a>
                 <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
             </div>
             <div class="update-clear">
@@ -96,6 +97,13 @@
                 <a class="btn btn-update" href="#">Update Shopping Cart</a>
             </div>
         </div>
+        @else
+            <div class="text-center" style="padding:30px 0;">
+                <h1>Your cart is empty!</h1>
+                <p>Add items to it now</p>
+                <a href="/shop" class="btn btn-success">Shop Now</a>
+            </div>
+        @endif
 
         <div class="wrap-iten-in-cart">
             <h1 class="title-box" style="border-bottom: 1px solid; padding-bottom:15px;">{{Cart::instance('saveForLater')->count()}} item(s) Saved For Later</h1>
